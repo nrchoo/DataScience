34bbac26a5078905280922e00c450733fe1b65ad
Nathan <nrchoo@gmail.com>
2016-12-13 00:24:47 -0800

    test

---
+++ Craig/DAT_SF_10/ThinkStats/Chapter_1/chap01ex_nc.py
@@ -0,0 +1,90 @@
+"""This file contains code for use with "Think Stats",
+by Allen B. Downey, available from greenteapress.com
+
+Copyright 2010 Allen B. Downey
+License: GNU GPLv3 http://www.gnu.org/licenses/gpl.html
+"""
+
+from __future__ import print_function
+
+from collections import defaultdict
+import numpy as np
+import sys
+
+import thinkstats2
+
+varpath = '/Users/nathanchoo/Desktop/Craig/DAT_SF_10/ThinkStats/ThinkStats2/code/'
+
+def ReadFemResp(dct_file= varpath + '2002FemResp.dct',
+                dat_file= varpath + '2002FemResp.dat.gz'):
+    """Reads the NSFG pregnancy data.
+
+    dct_file: string file name
+    dat_file: string file name
+
+    returns: DataFrame
+    """
+    dct = thinkstats2.ReadStataDct(dct_file)
+    df = dct.ReadFixedWidth(dat_file, compression='gzip')
+    #CleanFemPreg(df)
+    return df
+
+
+def CleanFemPreg(df):
+    """Recodes variables from the pregnancy frame.
+
+    df: DataFrame
+    """
+    # mother's age is encoded in centiyears; convert to years
+    df.agepreg /= 100.0
+
+    # birthwgt_lb contains at least one bogus value (51 lbs)
+    # replace with NaN
+    df.loc[df.birthwgt_lb > 20, 'birthwgt_lb'] = np.nan
+
+    # replace 'not ascertained', 'refused', 'don't know' with NaN
+    na_vals = [97, 98, 99]
+    df.birthwgt_lb.replace(na_vals, np.nan, inplace=True)
+    df.birthwgt_oz.replace(na_vals, np.nan, inplace=True)
+    df.hpagelb.replace(na_vals, np.nan, inplace=True)
+
+    df.babysex.replace([7, 9], np.nan, inplace=True)
+    df.nbrnaliv.replace([9], np.nan, inplace=True)
+
+    # birthweight is stored in two columns, lbs and oz.
+    # convert to a single column in lb
+    # NOTE: creating a new column requires dictionary syntax,
+    # not attribute assignment (like df.totalwgt_lb)
+    df['totalwgt_lb'] = df.birthwgt_lb + df.birthwgt_oz / 16.0
+
+    # due to a bug in ReadStataDct, the last variable gets clipped;
+    # so for now set it to NaN
+    df.cmintvw = np.nan
+
+
+def MakePregMap(df):
+    """Make a map from caseid to list of preg indices.
+
+    df: DataFrame
+
+    returns: dict that maps from caseid to list of indices into preg df
+    """
+    d = defaultdict(list)
+    for index, caseid in df.caseid.iteritems():
+        d[caseid].append(index)
+    return d
+
+
+def main(script):
+    """Tests the functions in this module.
+
+    script: string script name
+    """
+    df = ReadFemResp()
+    print(df.shape)
+
+
+    print('%s: All tests passed.' % script)
+
+if __name__ == '__main__':
+    main(*sys.argv)
